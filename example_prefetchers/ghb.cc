4c45eb76b313209c57fee45c69da9a3810288c4a
TomLan42 <lhc980616@gmail.com>
2024-04-21 20:44:17 +0800

    finish ghb g/ac prefetcher

--- example_prefetchers/ghb.cc
+++ example_prefetchers/ghb.cc
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
-// Define the struct
+
+typedef struct index_table_entry {
+    int addr_chain_len;
+    int addr_head_index;
+} index_table_entry_t;
+
+
 typedef struct ghb_entry {
     unsigned long long int addr;
     int last_addr_ptr;
@@ -9,48 +16,86 @@ typedef struct ghb_entry {
 
 class GHB {
 private:
+    std::unordered_map<unsigned long long int, index_table_entry_t> index_table;
     std::vector<ghb_entry_t> buffer;
     int capacity;
-    int head;     // head points to the newest element
-    int tail;     // tail points to the oldest element
+    int head;     // head points to the next write position
     int size;
 
 public:
     // Constructor
-    GHB(int capacity) : capacity(capacity), head(0), tail(0), size(0) {
+    GHB(int capacity) : capacity(capacity), head(0), size(0) {
         buffer.resize(capacity);
     }
 
     // Destructor
     ~GHB() {}
 
-    // Function to add an entry to the buffer
     void add_entry(unsigned long long int addr) {
+      // Init new entry
       ghb_entry_t entry;
-      if (size == 0) 
-      {
-        buffer[head] = entry;
-      } 
-      else
-      {
-        head = (head + 1) % capacity;
-        buffer[head] = entry;
-        if (head == tail) {
-          tail = (tail + 1) % capacity;
-        }
-      }
-      size = (size + 1) % capacity;
-
-      buffer[head].addr = addr;
+      entry.addr = addr;
       // find the most recent element with the same addr
       int last_ptr = -1;
-      for (int i = (head - 1) % capacity; i != (tail - 1) % capacity; i = (i - 1) % capacity) {
-        if (buffer[i].addr == addr) {
-            last_ptr = i;
+      for (int count = 0; count < size - 1; count++) {
+        int index = (head - 1 - count) % capacity;
+        if (buffer[index].addr == addr) {
+            last_ptr = index;
             break;
         }
       }
       entry.last_addr_ptr = last_ptr;
+
+
+      // Allocate new entry
+      ghb_entry_t replaced_entry =  buffer[head];
+      buffer[head] = entry;
+      head = (head + 1) % capacity;
+      if (size < capacity) {
+        size++;
+      }
+
+      // Update index table
+      if (replaced_entry.addr != 0) {
+        auto it = index_table.find(replaced_entry.addr);
+        if (it != index_table.end()) {
+          it->second.addr_chain_len--;
+          if (it->second.addr_chain_len == 0) {
+            index_table.erase(replaced_entry.addr);
+          }
+        }
+      }
+
+      auto it = index_table.find(entry.addr);
+      if (it != index_table.end()) {
+        it->second.addr_chain_len++;
+      } else {
+        index_table_entry_t index_table_entry;
+        index_table_entry.addr_chain_len = 1;
+        index_table_entry.addr_head_index = head - 1;
+        index_table[entry.addr] = index_table_entry;
+      }
+    }
+
+    std::vector<int> get_entries_by_addr(unsigned long long int addr, int width) {
+      std::vector<int> result;
+     
+      auto it = index_table.find(addr);
+      if (it == index_table.end()) {
+        return result;
+      }
+
+      int cur_index = it->second.addr_head_index;
+
+      for (int i = 0; i < it->second.addr_chain_len && i < width; i++) {
+        result.push_back(cur_index);
+        cur_index = buffer[i].last_addr_ptr;
+      }
+      return result;
+    }
+
+    ghb_entry_t get_entry_by_index(int index) {
+      return buffer[index];
     }
 
     bool is_full() {
--- example_prefetchers/ghb_g_ac_prefetcher.cc
+++ example_prefetchers/ghb_g_ac_prefetcher.cc
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "../inc/prefetcher.h"
 #include "ghb.cc"
+#include <set>
 
-#define INDEX_TABLE_LENGTH 32
 #define GHB_LENGTH 124
+#define PREFETCH_WIDTH 2
+#define PREFETCH_DEPTH 2
 
 GHB ghb;
 
@@ -21,14 +23,30 @@ void l2_prefetcher_operate(int cpu_num, unsigned long long int addr, unsigned lo
   // uncomment this line to see all the information available to make prefetch decisions
   //printf("(0x%llx 0x%llx %d %d %d) ", addr, ip, cache_hit, get_l2_read_queue_occupancy(0), get_l2_mshr_occupancy(0));
 
-
   ghb.add_entry(addr);
 
+  if (!cache_hit) {
+    std::set<unsigned long long int> prefetch_addr_set;
 
-  // trigger if cache miss
+    // get width
+    std::vector<int> width_indices = ghb.get_entries_by_addr(addr, PREFETCH_WIDTH);
+ 
+    // get depth
+    for (int i = 0; i < width_indices.size(); ++i) {
+      int index = width_indices[i];
+      for (int j = 0; j < PREFETCH_DEPTH; j++) {
+        ghb_entry_t entry = ghb.get_entry_by_index((index - 1 - j) % GHB_LENGTH);
+        prefetch_addr_set.insert(entry.addr);
+      }
+    }
 
 
+    for (int prefetch_addr : prefetch_addr_set) {
+      l2_prefetch_line(0, addr, prefetch_addr, FILL_L2);
+    }
+  }
 
+  return;
 }
 
 void l2_cache_fill(int cpu_num, unsigned long long int addr, int set, int way, int prefetch, unsigned long long int evicted_addr)
